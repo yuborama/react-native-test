4f4ea212206251c60568d0beff74204f45438d66
Yuborama <davidjohan2@hotmail.com>
2026-02-22 14:59:12 -0500

    feat: Rename "Explore" tab to "Meals" and update icon to magnifying glass

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -16,6 +16,7 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'fork.knife': 'restaurant',
+  'magnifyingglass': 'search',
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
